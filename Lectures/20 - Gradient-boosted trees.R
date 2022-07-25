8172517cfc903b1bc44012d3c15c241d0f6aa869
Amor Ai <aa@macbook-pro-13.wifi.local.cmu.edu>
2022-07-25 11:21:43 -0400

    L20 boosted trees update

--- Lectures/20 - Gradient-boosted trees.R
+++ Lectures/20 - Gradient-boosted trees.R
@@ -12,37 +12,51 @@ model_mlb_data <- mlb_data %>%
 head(model_mlb_data)
 
 
-# Try GBM -----------------------------------------------------------------
+# Fitting XGBoost -----------------------------------------------------------------
 
 library(xgboost)
-xgboost_tune_grid <- expand.grid(nrounds = seq(from = 20, to = 200, by = 20),
-                                 eta = c(0.025, 0.05, 0.1, 0.3), gamma = 0,
-                                 max_depth = c(1, 2, 3, 4), colsample_bytree = 1,
-                                 min_child_weight = 1, subsample = 1)
+xgboost_tune_grid <- expand.grid(nrounds = seq(from = 20, to = 200, by = 20), #search over trees
+                                 eta = c(0.025, 0.05, 0.1, 0.3), #learning rate
+                                 gamma = 0, 
+                                 max_depth = c(1, 2, 3, 4), #arbitrary
+                                 colsample_bytree = 1, #how many random samples (none)
+                                 min_child_weight = 1, #how many observations in a leaf node
+                                 subsample = 1) #use stochastic samples (no)
+#5-fold cross validation
+library(caret)
 xgboost_tune_control <- trainControl(method = "cv", number = 5, verboseIter = FALSE)
+
+#Caret way of tuning
 set.seed(1937)
-xgb_tune <- train(x = as.matrix(dplyr::select(model_mlb_data, -war)),
-                  y = model_mlb_data$war, trControl = xgboost_tune_control,
+xgb_tune <- train(x = as.matrix(dplyr::select(model_mlb_data, -war)), #all variables, drop response
+                  y = model_mlb_data$war, 
+                  trControl = xgboost_tune_control,
                   tuneGrid = xgboost_tune_grid, 
-                  objective = "reg:squarederror", method = "xgbTree",
-                  verbose = TRUE)
+                  method = "xgbTree",
+                  objective = "reg:squarederror", 
+                  verbose = FALSE) 
+#Summary
+xgb_tune$finalModel
 
 # Extreme grading boosting - XGBoost
 
 xgb_fit_final <- xgboost(data = as.matrix(dplyr::select(model_mlb_data, -war)),
-                         label = model_mlb_data$war, objective = "reg:squarederror",
-                         nrounds = xgb_tune$bestTune$nrounds,
+                         label = model_mlb_data$war, #response
+                         objective = "reg:squarederror",
+                         nrounds = xgb_tune$bestTune$nrounds, #num of trees
                          params = as.list(dplyr::select(xgb_tune$bestTune,
-                                                        -nrounds)), 
+                                              -nrounds)), #hyper-parameters in list form
                          verbose = 0)
 
 # Variable importance
+library(vip)
 vip(xgb_fit_final) + theme_bw()
 
-# Partial dependence plots
+# Partial dependence plots - viewing marginal relationship with particular variables (off)
 library(pdp)
 partial(xgb_fit_final, pred.var = "off", train = as.matrix(dplyr::select(model_mlb_data, -war)),
         plot.engine = "ggplot2", plot = TRUE,
         type = "regression") + theme_bw()
+#problem with correlations
 
 
